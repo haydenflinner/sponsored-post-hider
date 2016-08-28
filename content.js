6fbab1d19e2020c633693aa54f9d9c81c226857f
Hayden Flinner <hayden@gatech.edu>
2016-08-27 20:33:35 -0400

    Removed outdated code.

--- content.js
+++ content.js
@@ -1,13 +1,9 @@
 setInterval(function hidePosts() {
-    var divs = document.getElementById("contentArea").getElementsByTagName('div');
     var elements = document.querySelectorAll('[data-testid^=fbfeed_story]');
     for (var i = 0; i < elements.length; i++) {
         var x = elements[i].querySelectorAll('[class^=uiStreamSponsoredLink]'); // Sponsored links
         if (x.length > 0) {
             elements[i].parentNode.removeChild(elements[i]);
-            console.log('Removed: ');
-            console.log(elements[i]);
         }
     }
 }, 2000);
-
